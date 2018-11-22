93212d27f1066d3ee9f204b77a2ce0bac6390518
Alexander Voronkov <ax.voronkov@gmail.com>
2018-11-22 13:09:23 +0300

    Add full auction program

--- Auction/Auction.cs
+++ Auction/Auction.cs
@@ -9,7 +9,7 @@ namespace PcallNamespace {
         Official Expload Auction Program
         */
 
-        public static void Main() { }
+        public static void Main(){ }
 
         /*
         PASS adresses of different games storage
@@ -23,7 +23,7 @@ namespace PcallNamespace {
             new Mapping<UInt32, Bytes>();
 
         // Add a new game address
-        public UInt32 addGame(Bytes address) {
+        public UInt32 addGame(Bytes address){
             // Only Auction Owner can do this
             assertIsAuctionOwner();
             // Add game address to the storage
@@ -32,8 +32,40 @@ namespace PcallNamespace {
         }
 
         // Get game address by its game id
-        private Bytes getGameAddress(UInt32 id) {
+        private Bytes getGameAddress(UInt32 id){
             return gamesAddresses.getDefault(id, Bytes.VOID_ADDRESS);
+        }
+
+        /*
+        Parsing lot objects
+        */
+
+        // Parse arguments into Lot object
+        private Lot ParseLot(
+            Bytes creator, UInt32 gameId, UInt32 assetId,
+            Bytes externalId, UInt32 startingPrice, UInt64 endTime
+        ){
+            var lot = new Lot();
+            lot.creator = creator;
+            lot.gameId = gameId;
+            lot.assetId = assetId;
+            lot.externalId = externalId;
+            lot.startingPrice = startingPrice;
+            lot.endTime = endTime;
+            return lot;
+        }
+
+        // Dump Lot into JSON
+        private string DumpLot(Lot lot){
+            return
+            "{" +
+                "\"creator\": \""       + BytesToHex(lot.creator)                    + "\"," +
+                "\"gameId\": \""        + System.Convert.ToString(lot.gameId)        + "\"," + 
+                "\"assetId\": \""       + System.Convert.ToString(lot.assetId)       + "\"," +
+                "\"externalId\": \""    + BytesToHex(lot.externalId)                 + "\"," +
+                "\"startingPrice\": \"" + System.Convert.ToString(lot.startingPrice) + "\"," +
+                "\"endTime\": \""       + System.Convert.ToString(lot.endTime)       + "\""  +
+            "}";
         }
 
         /*
@@ -47,6 +79,16 @@ namespace PcallNamespace {
         public Mapping<UInt32, Lot> Lots =
             new Mapping<UInt32, Lot>();
 
+        // Get lot by its id
+        private Lot getLot(UInt32 id){
+            return Lots.getDefault(id, new Lot());
+        }
+
+        // Get jsonified lot data
+        public string getLotData(UInt32 id){
+            return DumpLot(getLot(id));
+        }
+
         /*
         Lot ids belonging to particular users storage
         */
@@ -59,7 +101,7 @@ namespace PcallNamespace {
         public Mapping<Bytes, UInt32> userLotsCount =
             new Mapping<Bytes, uint>();
 
-        public UInt32 getUserLotId(Bytes address, UInt32 number) {
+        public UInt32 getUserLotId(Bytes address, UInt32 number){
             // We can't get more lots than user has
             if(number >= userLotsCount.getDefault(address, 0)){
                 Error.Throw("This asset doesn't exist!");
@@ -69,7 +111,7 @@ namespace PcallNamespace {
         }
 
         // Get the key for userLots mapping
-        private string getUserLotKey(Bytes address, UInt32 number) {
+        private string getUserLotKey(Bytes address, UInt32 number){
             return (BytesToHex(address) + System.Convert.ToString(number));
         }
 
@@ -86,23 +128,23 @@ namespace PcallNamespace {
             new Mapping<string, UInt32>();
 
         // IMPORTANT: Asset id = External Asset id (see PASS.cs)
-        public UInt32 getAssetLotId(UInt32 gameId, Bytes assetId, UInt32 number) {
+        public UInt32 getAssetLotId(UInt32 gameId, Bytes externalId, UInt32 number){
             // We can't get more lots than asset has
-            if(number >= assetLotsCount.getDefault(getAssetCountKey(gameId, assetId), 0)){
+            if(number >= assetLotsCount.getDefault(getAssetCountKey(gameId, externalId), 0)){
                 Error.Throw("This asset doesn't exist!");
             }
-            string key = getAssetLotKey(gameId, assetId, number);
+            string key = getAssetLotKey(gameId, externalId, number);
             return assetLots.get(key);
         }
 
         // Get the key for assetLotsCount mapping
-        private string getAssetCountKey(UInt32 gameId, Bytes assetId) {
-            return (System.Convert.ToString(gameId) + BytesToHex(assetId));
+        private string getAssetCountKey(UInt32 gameId, Bytes externalId){
+            return (System.Convert.ToString(gameId) + BytesToHex(externalId));
         }
 
         // Get the key for assetLots mapping
-        private string getAssetLotKey(UInt32 gameId, Bytes assetId, UInt32 number) {
-            return (System.Convert.ToString(gameId) + BytesToHex(assetId) + System.Convert.ToString(number));
+        private string getAssetLotKey(UInt32 gameId, Bytes externalId, UInt32 number){
+            return (System.Convert.ToString(gameId) + BytesToHex(externalId) + System.Convert.ToString(number));
         }
 
         /*
@@ -113,15 +155,210 @@ namespace PcallNamespace {
         // (if it's a call from game's server)
         private void assertIsAuctionOwner(){
             if (Info.Sender() != Info.ProgramAddress()){
-                Error.Throw("Only owner of the program can do that.");
+                Error.Throw("Only program owner can do this.");
             }
+        }
+
+        // Checks if caller owns a particular asset
+        private void assertIsItemOwner(UInt32 gameId, UInt32 assetId){
+            Bytes gameAddress = getGameAddress(gameId);
+            Bytes assetOwner = ProgramHelper.Program<PASS>(gameAddress).getXCAssetOwner(assetId);
+            if(Info.Sender() != assetOwner){
+                Error.Throw("Only asset owner can do this.");
+            }
+        }
+
+        // Checks if caller is a creator of particular lot
+        private void assertIsLotCreator(UInt32 lotId){
+            if(Info.Sender() != getLot(lotId).creator){
+                Error.Throw("Only lot creator can do this.");
+            }
+        }
+
+        /*
+        Interacting with the storage
+        */
+
+        /// <summary>
+        /// Creates a new lot with desired parameters,
+        /// asset ownership is transfered to auction wallet.
+        /// </summary>
+        /// <param name="gameId"> Id of the game the asset is from </param>
+        /// <param name="assetId"> Blockchain id of the asset sold (see PASS.cs) </param>
+        /// <param name="startingPrice"> Starting price of the lot, can't equal 0 </param>
+        /// <param name="endTime"> UNIX timestamp for the end of the sales </param>
+        /// <returns>
+        /// Created lot id
+        /// </returns>
+        /// <remarks>
+        /// If the lot is closed by its creator, the bids are given back and the
+        /// asset is returned to the owner. If the lot is not closed before the timeout,
+        /// highest bidder gets the asset and lot creator gets the highest bid.
+        /// If endTime timestamp is in the past (regarding lot timeout daemon),
+        /// the lot is to be closed almost instantly and the lot 
+        /// is not to be shown in Expload acution UI.
+        /// </remarks>
+        public UInt32 createLot(
+            UInt32 gameId, UInt32 assetId, 
+            UInt32 startingPrice, UInt64 endTime
+        ){
+            // Check if user has the item he wants to sell
+            assertIsItemOwner(gameId, assetId);
+
+            // Check if the starting price is legit
+            if(startingPrice == 0){
+                Error.Throw("Starting price can't equal 0.");
+            }
+
+            // Get game address
+            Bytes gameAddress = getGameAddress(gameId);
+
+            // Get item external id
+            Bytes externalId = ProgramHelper.Program<PASS>(gameAddress).getXCAssetExternalId(assetId);
+
+            // Transfer the asset to auction's wallet (so user can't use it)
+            ProgramHelper.Program<PASS>(gameAddress).TransferXCAsset(assetId, Info.ProgramAddress());
+
+            // Create lot object and put it into main storage
+            Lot lot = ParseLot(Info.Sender(), gameId, assetId, externalId, startingPrice, endTime);
+            UInt32 lotId = lastLotId;
+            Lots.put(lastLotId++, lot);
+
+            // Put the lot into user storage
+            UInt32 userStorageLastId = userLotsCount.getDefault(Info.Sender(), 0);
+            string userLotsKey = getUserLotKey(Info.Sender(), userStorageLastId);
+            userLots.put(userLotsKey, lotId);
+            userLotsCount.put(Info.Sender(), userStorageLastId + 1);
+
+            // Put the lot into particular asset storage
+            string assetLotsCountKey = getAssetCountKey(gameId, externalId);
+            UInt32 assetCount = assetLotsCount.getDefault(assetLotsCountKey, 0);
+            string assetLotsKey = getAssetLotKey(gameId, externalId, assetCount);
+            assetLots.put(assetLotsKey, lotId);
+            assetLotsCount.put(assetLotsKey, assetCount+1);
+
+            // TODO: add event
+
+            return lotId;
+        }
+
+        // Minimum amount of lot bid increment
+        public UInt32 minBidIncrement = 5;
+
+        // Sets new minimum bid increment
+        // Can only be done by program owner
+        public void setMinBidIncrement(UInt32 increment){
+            assertIsAuctionOwner();
+            minBidIncrement = increment;
+        }
+
+        /// <summary>
+        /// Places a new bid on desired lot
+        /// </summary>
+        /// <param name="lotId"> Id of the lot </param>
+        /// <param name="bid"> Desired bid </param>
+        /// <remarks>
+        /// If there haven't been any bids on the lot before, the bid
+        /// should be equal to the starting price or be greater than it.
+        /// When one's bid is outbid, the coins are returned.
+        /// </remarks>
+        public void makeBid(UInt32 lotId, UInt32 bid){
+            // Get the lot object
+            Lot lot = getLot(lotId);
+
+            // Check if the lot is not closed yet
+            if(lot.closed){
+                Error.Throw("The lot is already closed.");
+            }
+
+            // Check if the bid is legit
+            if(!(
+                bid >= lot.startingPrice && lot.lastBid == 0 
+                || lot.lastBid != 0 && bid >= lot.lastBid + minBidIncrement
+            )) {
+                Error.Throw("The bid is not legit.");
+            }
+            
+            // Transfer the outbid bet back to the bidder
+            if(lot.lastBid != 0){
+                Actions.TransferFromProgram(lot.lastBidder, lot.lastBid);
+            }
+
+            // Transfer new bid to the auction
+            Actions.Transfer(Info.ProgramAddress(), bid);
+
+            // Alter the lot state and write it to the storage
+            lot.lastBid = bid;
+            lot.lastBidder = Info.Sender();
+            Lots.put(lotId, lot);
+        }
+
+        /// <summary>
+        /// Cancel the bidding, return the bids to bidders
+        /// and the asset to its owner.
+        /// </summary>
+        /// <param name="lotId"> Id of the lot </param>
+        /// <remarks>
+        /// If the lot is closed, it is not to be shown
+        /// in Expload Autction UI (except for lot creator's lot history).
+        /// The lot is permanently closed and archieved, it can't be reopened.
+        /// </remarks>
+        public void closeLot(UInt32 lotId){
+            // Check if sender has permission to do this
+            assertIsLotCreator(lotId);
+
+            // Get the lot object
+            Lot lot = getLot(lotId);
+
+            // Change the lot state and write it to the storage
+            lot.closed = true;
+            Lots.put(lotId, lot);
+
+            // Return the last bid to the bidder
+            Actions.TransferFromProgram(lot.lastBidder, lot.lastBid);
+
+            // Return the asset to the owner
+            Bytes gameAddress = getGameAddress(lot.gameId);
+            ProgramHelper.Program<PASS>(gameAddress).TransferXCAsset(lot.assetId, lot.creator);
+        }
+
+        /// <summary>
+        /// Special method for auction's timeout daemon program
+        /// which constantly looks for expired lots.
+        /// When an expired lot is found, the highest bidder
+        /// gets the asset and the lot creator gets the highest bid.
+        /// </summary>
+        /// <param name="lotId"> Id of the lot </param>
+        /// <remarks>
+        /// Though the timeout worker may be a little late for 
+        /// contract expiration (max - couple of seconds),
+        /// outdated lots are not shown in Expload UI, and
+        /// latency is too small and random for it to be exploited.
+        /// </remarks>
+        public void timeoutLot(UInt32 lotId){
+            // Only the daemon script (program owner) can do this
+            assertIsAuctionOwner();
+
+            // Get the lot object
+            Lot lot = getLot(lotId);
+
+            // Change the lot state and write it to the storage
+            lot.closed = true;
+            Lots.put(lotId, lot);
+
+            // Give the coins to lot creator
+            Actions.TransferFromProgram(lot.creator, lot.lastBid);
+
+            // Transfer asset to the highest bidder
+            Bytes gameAddress = getGameAddress(lot.gameId);
+            ProgramHelper.Program<PASS>(gameAddress).TransferXCAsset(lot.assetId, lot.lastBidder);
         }
 
         /*
         Some string & bytes operations
         */
 
-        private string HexPart(int b) {
+        private string HexPart(int b){
             if (b == 0)
                 return "0";
             else if (b == 1)
@@ -165,7 +402,7 @@ namespace PcallNamespace {
         private string BytesToHex(Bytes bytes)
         {
             string res = "";
-            for (int i = 0; i < bytes.Length(); i++) {
+            for (int i = 0; i < bytes.Length(); i++){
                 res += ByteToHex(bytes[i]);
             }
             return res;
@@ -178,30 +415,30 @@ namespace PcallNamespace {
         */
 
         // Address of lot creator
-        Bytes creator;
+        public Bytes creator;
+
+        // Id of the game the asset is from
+        public UInt32 gameId;
 
         // Blockchain id of the asset sold (see PASS.cs)
-        UInt32 id;
+        public UInt32 assetId;
 
         // External game id of the asset sold (see PASS.cs)
-        UInt32 externalId;
+        public Bytes externalId;
 
         // Starting price of the asset
-        UInt32 startingPrice;
+        public UInt32 startingPrice;
 
         // Last (highest) lot bid
-        UInt32 lastBid;
+        public UInt32 lastBid = 0;
 
         // The owner of the last bid
-        Bytes lastBidder;
+        public Bytes lastBidder = Bytes.VOID_ADDRESS;
 
-        // UNIX timestamp for lot start
-        UInt64 StartTime;
-
-        // Lot duration, seconds
-        UInt32 Duration;
+        // UNIX timestamp for lot end
+        public UInt64 endTime;
 
         // If the lot is already closed
-        bool closed = false;
+        public bool closed = false;
     }
 }
--- PASS/PASS.cs
+++ PASS/PASS.cs
@@ -52,7 +52,7 @@ namespace PcallNamespace {
         only GT methods should be use for GT assets, similarly for XC assets.
         */
 
-        public static void Main() { }
+        public static void Main(){ }
 
         // Parse arguments into Asset object
         private Asset ParseAsset(Bytes owner, Bytes externalId, Bytes metaId){
@@ -92,6 +92,14 @@ namespace PcallNamespace {
             return DumpAsset(getGTAsset(id));
         }
 
+        public Bytes getGTAssetOwner(UInt32 id){
+            return getGTAsset(id).owner;
+        }
+
+        public Bytes getGTAssetExternalId(UInt32 id){
+            return getGTAsset(id).externalId;
+        }
+
         // Mapping storing XC assets
         // This mapping's key is asset's blockchain id
         public Mapping<UInt32, Asset> XCAssets =
@@ -105,6 +113,14 @@ namespace PcallNamespace {
             return DumpAsset(getXCAsset(id));
         }
 
+        public Bytes getXCAssetOwner(UInt32 id){
+            return getXCAsset(id).owner;
+        }
+
+        public Bytes getXCAssetExternalId(UInt32 id){
+            return getXCAsset(id).externalId;
+        }
+
         // Mapping storing GT assets ids belonging to a user
         // Key is the concatenation of user address and asset number in his storage
         public Mapping<string, UInt32> GTUsersAssetIds =
@@ -115,12 +131,12 @@ namespace PcallNamespace {
             new Mapping<Bytes, UInt32>();
 
         // Get user's asset counter
-        public UInt32 getGTUsersAssetCount(Bytes address) {
+        public UInt32 getGTUsersAssetCount(Bytes address){
             return GTUsersAssetCount.getDefault(address, 0);
         }
 
         // Get one of user's GT assets
-        public UInt32 getUsersGTAssetId(Bytes address, UInt32 number) {
+        public UInt32 getUsersGTAssetId(Bytes address, UInt32 number){
             // We can't get more assets than user owns
             if(number >= GTUsersAssetCount.getDefault(address, 0)){
                 Error.Throw("This asset doesn't exist!");
@@ -138,12 +154,12 @@ namespace PcallNamespace {
             new Mapping<Bytes, UInt32>();
 
         // Get user's asset counter
-        public UInt32 getXCUsersAssetCount(Bytes address) {
+        public UInt32 getXCUsersAssetCount(Bytes address){
             return XCUsersAssetCount.getDefault(address, 0);
         }
 
         // Get one of user's XC assets
-        public UInt32 getUsersXCAssetId(Bytes address, UInt32 number) {
+        public UInt32 getUsersXCAssetId(Bytes address, UInt32 number){
             // We can't get more assets than user owns
             if(number >= XCUsersAssetCount.getDefault(address, 0)){
                 Error.Throw("This asset doesn't exist!");
@@ -333,7 +349,7 @@ namespace PcallNamespace {
         Some string & bytes operations
         */
 
-        private string HexPart(int b) {
+        private string HexPart(int b){
             if (b == 0)
                 return "0";
             else if (b == 1)
@@ -369,15 +385,13 @@ namespace PcallNamespace {
             return "";
         }
 
-        private string ByteToHex(byte b)
-        {
+        private string ByteToHex(byte b){
             return HexPart(b / 16) + HexPart(b % 16);
         }
 
-        private string BytesToHex(Bytes bytes)
-        {
+        private string BytesToHex(Bytes bytes){
             string res = "";
-            for (int i = 0; i < bytes.Length(); i++) {
+            for (int i = 0; i < bytes.Length(); i++){
                 res += ByteToHex(bytes[i]);
             }
             return res;
